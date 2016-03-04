c73061278c2c9d06a43f35e1e7b1089fd74dc20c
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-03-04 20:34:46 +1300

    added heading stripe and made the category links work

--- index.js
+++ index.js
@@ -2,41 +2,46 @@ var request = require('superagent')
 var $ = require('jquery')
 
 $("#plastic-injection").click(function() {
-	request
-		.get('/plastics')
-		.end(function(err, res){
-			console.log('plastic')
-	})
+	// request
+	// 	.get('/plastics')
+	// 	.end(function(err, res){
+	// 		console.log('plastic')
+	// })
+	window.location='/plastics'
 })
 
 $("#laser-cutting").click(function() {
-	request
-		.get('/laser')
-		.end(function(err, res){
-			console.log('laser cut')
-	})
+	// request
+	// 	.get('/laser')
+	// 	.end(function(err, res){
+	// 		console.log('laser cut')
+	// })
+	window.location='/laser'
 })
 
 $("#prototype").click(function() {
-	request
-		.get('/prototype')
-		.end(function(err, res){
-			console.log('prototype')
-	})
+	// request
+	// 	.get('/prototype')
+	// 	.end(function(err, res){
+	// 		console.log('prototype')
+	// })
+	window.location='/prototype'
 })
 
 $("#3d-printing").click(function() {
-	request
-		.get('/3Dprint')
-		.end(function(err, res){
-			console.log('3d printing')
-	})
+	// request
+	// 	.get('/3Dprint')
+	// 	.end(function(err, res){
+	// 		console.log('3d printing')
+	// })
+	window.location='/3Dprint'
 })
 
 $("#clear-filters").click(function() {
-	request
-	.get('/')
-	.end(function(err, res) {
-		console.log('clear filter')
-	})
+	// request
+	// .get('/')
+	// .end(function(err, res) {
+	// 	console.log('clear filter')
+	// })
+	window.location='/'
 })
