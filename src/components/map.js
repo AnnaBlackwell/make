b8a6540bb603d8eb39ed72d41b1bbd98558180c7
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-03-08 20:22:42 +1300

    Sign in and sign up added. Also add contact form:

---
+++ src/components/map.js
@@ -0,0 +1,14 @@
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYWNibGFja3dlbGwiLCJhIjoiY2lsaXAyeWFvM2JpNHVka3AxcjZpdDFuayJ9.ifS4N66JQmRHPEHkTbvs6g';
+// var map = new mapboxgl.Map({
+//     container: 'map', // container id
+//     style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+//     center: [-74.50, 40], // starting position
+//     zoom: 1 // starting zoom
+// });
+
+// map.on('style.load', function () {
+//     map.addSource('RPD_Parks', {
+//         type: 'vector',
+//         url: 'mapbox://mapbox.3o7ubwm8'
+//     });
+// });
